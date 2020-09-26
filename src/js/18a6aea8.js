4c42c5d25102ffcd46325718aee230032ff9e939
teenari <teenari@users.noreply.github.com>
2020-09-25 18:04:52 -0700

    Update 18a6aea8.js

--- src/js/18a6aea8.js
+++ src/js/18a6aea8.js
@@ -1393,11 +1393,7 @@ $(document).ready(async () => {
                 if(e.target.className === 'threedots') return;
                 displayName = account;
             });
-            $(`[id="${account}"]`).hover(
-                () => $(`[id="${account}"]`).css({'boxShadow': `${twofive} 0px 0px 29px`}),
-                () => $(`[id="${account}"]`).css({'boxShadow': `none`})
-            );
-            const settings = async (e) => {
+            $(`[id="${account}buttonsmIW"]`).click((e) => {
                 const selector = `[id="${account}"]`;
                 $('.accounts').children(`[id!="${account}"]`).fadeOut();
                 $(selector).css('cursor', 'auto').css('top', '').css('left', '').css('position', 'relative').css('width', '171px').css('height', '204px').animate({top: '4vh', left: '-1vh', width: '313px', height: '218px'}).children()[0].style.height = '154px';
@@ -1422,9 +1418,9 @@ $(document).ready(async () => {
                     await system.editBot(accountO.repl, name, skin || accountO.cid, accountO.name);
                     $('.accounts').children(`[id!="${account}"]`).fadeIn();
                     $(`[id="${account}"]`).children().eq(0).children('div').remove();
-                    $(`[id="${account}"]`).children().eq(0).children().eq(0).after(`<div class="threedots" id="${account}buttonsmIW">⠇</div>`);
                     $(`[id="${account}"]`).children().eq(1)[0].outerHTML = `<div style="color: ${$(`[id="${account}"]`).children().eq(1).css('color')};">${name}</div>`;
                     $(`[id="${account}"]`).children().eq(0).css('height', '100px');
+                    $(`[id="${account}"]`).children().eq(0).children().eq(0).after(`<div class="threedots" id="${account}buttonsmIW">⠇</div>`);
                     $(`[id="${account}"]`).css('cursor', 'pointer').css('height', '').css('top', '').css('left', '').css('width', '171px');
                     accountsNames.accounts.find(e => e.name === accountO.name && accountO.repl === accountO.repl).name;
                     $(`[id="${account}"]`).click((e) => {
@@ -1497,12 +1493,11 @@ $(document).ready(async () => {
                         $('[id="menu-create"]').animate({left: '312.667px'}, 50);
                     }
                 });
-            }
-            console.log($(`[id="${account}buttonsmIW"]`));
-            $(`[id="${account}buttonsmIW"]`).click(async (e) => {
-                console.log('s');
-                await settings(e);
             });
+            $(`[id="${account}"]`).hover(
+                () => $(`[id="${account}"]`).css({'boxShadow': `${twofive} 0px 0px 29px`}),
+                () => $(`[id="${account}"]`).css({'boxShadow': `none`})
+            );
         });
         used.push(cid);
     };
